086aefddc47896c83a935f05cc4d28245a3caca0
davidwwang <davidwwang@tencent.com>
2022-11-23 11:55:17 +0800

    fix bug, ple pytest pass

--- tensorflow/deeprec/models/multitask/ple.py
+++ tensorflow/deeprec/models/multitask/ple.py
@@ -1,18 +1,20 @@
 # -*- coding:utf-8 -*-
 
 import tensorflow as tf
-from tensorflow.python.keras.models import Model 
+from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.layers import Dense, Lambda
 
+from ...utils.feature_column import build_input_features, input_from_feature_columns
 from ...layers.core import PredictionLayer, DNN
 from ...layers.utils import combined_dnn_input, reduce_sum
-from ...utils.feature_column import build_input_features, input_from_feature_columns
 
 def PLE(dnn_feature_columns, shared_expert_num=1, specific_expert_num=1, num_levels=2,
-       expert_dnn_hidden_units=(256,), tower_dnn_hidden_units=(64,), gate_dnn_hidden_units=(),
-       l2_reg_embedding=0.0001, l2_reg_dnn=0, seed=1024, dnn_dropout=0, dnn_activation='relu',
-       dnn_use_bn = False, task_types=('binary', 'binary'), task_names = ('ctr', 'cvr')):
+        expert_dnn_hidden_units=(256,), tower_dnn_hidden_units=(64,), gate_dnn_hidden_units=(),
+        l2_reg_embedding=0.00001,
+        l2_reg_dnn=0, seed=1024, dnn_dropout=0, dnn_activation='relu', dnn_use_bn=False,
+        task_types=('binary', 'binary'), task_names=('ctr', 'ctcvr')):
     """Instantiates the multi level of Customized Gate Control of Progressive Layered Extraction architecture.
+
     :param dnn_feature_columns: An iterable containing all the features used by deep part of the model.
     :param shared_expert_num: integer, number of task-shared experts.
     :param specific_expert_num: integer, number of task-specific experts.
@@ -28,9 +30,9 @@ def PLE(dnn_feature_columns, shared_expert_num=1, specific_expert_num=1, num_lev
     :param dnn_use_bn: bool. Whether use BatchNormalization before activation or not in DNN.
     :param task_types: list of str, indicating the loss of each tasks, ``"binary"`` for  binary logloss, ``"regression"`` for regression loss. e.g. ['binary', 'regression']
     :param task_names: list of str, indicating the predict target of each tasks
+
     :return: a Keras model instance.
     """
-    
     num_tasks = len(task_names)
     if num_tasks <=1 :
         raise ValueError('num_tasks must be greater than 1')
@@ -42,7 +44,7 @@ def PLE(dnn_feature_columns, shared_expert_num=1, specific_expert_num=1, num_lev
             raise ValueError("task_type must be binary or regression. {} is illegal".format(task_type))
     
     features = build_input_features(dnn_feature_columns)
-    input_list = list(features.values())
+    inputs_list = list(features.values())
     
     sparse_embedding_list, dense_value_list = input_from_feature_columns(features, dnn_feature_columns, l2_reg_embedding, seed)
     dnn_input = combined_dnn_input(sparse_embedding_list, dense_value_list)
@@ -102,15 +104,16 @@ def PLE(dnn_feature_columns, shared_expert_num=1, specific_expert_num=1, num_lev
             gate_out = Lambda(lambda x:tf.expand_dims(x, axis=-1))(gate_out)
             
             # gate multiply the expert
-            gate_mul_expert = Lambda(lambda x: reduce_sum(x[0]*x[1], axis=1, keep_dims=False),
-                                    name=level_name + 'gate_softmax_shared')([expert_concat, gate_out])
-            
+            gate_mul_expert = Lambda(lambda x: reduce_sum(x[0] * x[1], axis=1, keep_dims=False),
+                                                     name=level_name + 'gate_mul_expert_shared')(
+                [expert_concat, gate_out])
+
             cgc_outs.append(gate_mul_expert)
 
         return cgc_outs
-    
+
     # Build Progressive Layered Extraction
-    ple_input = [dnn_input] *(num_tasks+1) # [task1, task2, ... taskn, shared task]
+    ple_inputs = [dnn_input] * (num_tasks + 1)  # [task1, task2, ... taskn, shared task]
     ple_outputs = []
     for i in range(num_levels):
         if i == num_levels -1 : # the last level
@@ -118,7 +121,7 @@ def PLE(dnn_feature_columns, shared_expert_num=1, specific_expert_num=1, num_lev
         else:
             ple_outputs = cgc_net(inputs=ple_inputs, level_name='level_' + str(i) + '_', is_last=False)
             ple_inputs = ple_outputs
-    
+
     task_outs = []
     for task_type, task_name, ple_out in zip(task_types,task_names, ple_outputs):
         # build tower layer
@@ -127,6 +130,6 @@ def PLE(dnn_feature_columns, shared_expert_num=1, specific_expert_num=1, num_lev
         logit = Dense(1, use_bias=False)(tower_output)
         output = PredictionLayer(task_type, name=task_name)(logit)
         task_outs.append(output)
-    
-    model = Model(inputs=input_list, output=task_outs)
+
+    model = Model(inputs=inputs_list, outputs=task_outs)
     return model
