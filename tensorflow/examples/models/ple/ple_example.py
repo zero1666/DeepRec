293ac524ac357178356c9640b336696229477dd9
davidwwang <davidwwang@tencent.com>
2022-11-29 20:23:50 +0800

    pass ple example

--- tensorflow/examples/models/ple/ple_example.py
+++ tensorflow/examples/models/ple/ple_example.py
@@ -17,7 +17,64 @@ from deeprec.models.multitask import PLE
 
 
 if __name__ == "__main__":
-    pass
-    
-	
+    data_path = os.path.dirname(root_path) + "/data/census-income.sample"
+    #print(data_path)
+    column_names = ['age', 'class_worker', 'det_ind_code', 'det_occ_code', 'education', 'wage_per_hour', 'hs_college',
+            'marital_stat', 'major_ind_code', 'major_occ_code', 'race', 'hisp_origin', 'sex', 'union_member',
+            'unemp_reason', 'full_or_part_emp', 'capital_gains', 'capital_losses', 'stock_dividends',
+            'tax_filer_stat', 'region_prev_res', 'state_prev_res', 'det_hh_fam_stat', 'det_hh_summ',
+            'instance_weight', 'mig_chg_msa', 'mig_chg_reg', 'mig_move_reg', 'mig_same', 'mig_prev_sunbelt',
+            'num_emp', 'fam_under_18', 'country_father', 'country_mother', 'country_self', 'citizenship',
+            'own_or_self', 'vet_question', 'vet_benefits', 'weeks_worked', 'year', 'income_50k']
+    data =  pd.read_csv(data_path, header=None, names=column_names)
+
+    data['label_income'] = data['income_50k'].map({' - 50000.': 0, ' 50000+.': 1})
+    data['label_marital'] = data['marital_stat'].apply(lambda x: 1 if x == ' Never married' else 0)
+    data.drop(labels=['income_50k', 'marital_stat'], axis=1, inplace=True)
+
+    columns = data.columns.values.tolist()
+    sparse_features = ['class_worker', 'det_ind_code', 'det_occ_code', 'education', 'hs_college', 'major_ind_code',
+            'major_occ_code', 'race', 'hisp_origin', 'sex', 'union_member', 'unemp_reason',
+            'full_or_part_emp', 'tax_filer_stat', 'region_prev_res', 'state_prev_res', 'det_hh_fam_stat',
+            'det_hh_summ', 'mig_chg_msa', 'mig_chg_reg', 'mig_move_reg', 'mig_same', 'mig_prev_sunbelt',
+            'fam_under_18', 'country_father', 'country_mother', 'country_self', 'citizenship',
+            'vet_question']
+    dense_features = [col for col in columns if
+            col not in sparse_features and col not in ['label_income', 'label_marital']]
+
+    data[sparse_features] = data[sparse_features].fillna('-1', )
+    data[dense_features] = data[dense_features].fillna(0, )
+
+    mms = MinMaxScaler(feature_range=(0,1))
+    data[dense_features] = mms.fit_transform(data[dense_features])
+
+    for feat in sparse_features:
+        lbe = LabelEncoder()
+        data[feat] = lbe.fit_transform(data[feat])
+
+    fixlen_feature_columns = [SparseFeat(feat,data[feat].max()+1, embedding_dim=4) for feat in sparse_features] \
+                                + [DenseFeat(feat, 1,) for feat in dense_features]
+
+    dnn_feature_colums = fixlen_feature_columns
+    linear_feature_colmns = fixlen_feature_columns
+
+    feature_names = get_feature_names(linear_feature_colmns + dnn_feature_colums)
+
+    # generate input data for model
+    train, test = train_test_split(data, test_size=0.2, random_state=2012)
+    train_model_input = {name:train[name] for name in feature_names}
+    test_model_input = {name:test[name] for name in feature_names}
+
+    # define Model, train, predict and evaluate
+    model = PLE(dnn_feature_colums, shared_expert_num=2, specific_expert_num=2, num_levels=2,
+            task_types=['binary','binary'], task_names=['label_income', 'label_marital'])
+    model.compile("adam", loss=["binary_crossentropy", "binary_crossentropy"],
+            metrics=['binary_crossentropy'],)
+
+    history = model.fit(train_model_input,[train['label_income'].values, train['label_marital'].values],
+            batch_size=256, epochs=10, verbose=2,validation_split=0.2)
+    pred_ans = model.predict(test_model_input,batch_size=256)
+
+    print("test income AUC", round(roc_auc_score(test['label_income'], pred_ans[0]), 4))
+    print("test marital AUC", round(roc_auc_score(test['label_marital'], pred_ans[1]), 4))
 
